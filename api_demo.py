3d6893fc3516c767628d89ae3788f7c5892e46d5
PajaspaceNet <elektrosoft.paja@gmail.com>
2025-01-19 07:03:22 +0100

    Oprava chyby portu

--- api_demo.py
+++ api_demo.py
@@ -7,5 +7,6 @@ def home():
     return render_template("index.html")  # Načte šablonu z `templates/index.html`
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000)
+#    app.run(host="0.0.0.0", port=8000)
+                  app.run()
 
