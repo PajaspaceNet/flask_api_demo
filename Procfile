1a4f3b42046ebe537f9483966f34aa8dacc1b54b
PajaspaceNet <elektrosoft.paja@gmail.com>
2025-01-19 07:51:22 +0100

    Update Gunicorn configuration for api_demo

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn api_demo:app
+web: gunicorn api_demo:application
--- api_demo.py
+++ api_demo.py
@@ -1,12 +1,12 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
+application = Flask(__name__)
 
-@app.route("/")
+@application.route("/")
 def home():
     return render_template("index.html")  # Načte šablonu z `templates/index.html`
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))  # Default port for local dev
-    app.run(host="0.0.0.0", port=port)
+    application.run(host="0.0.0.0", port=port)
 
